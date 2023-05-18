3a303e33f1b86f79adae98df1c52b221bf496f62
anastasiya-lazar <leloneya@gmail.com>
2023-05-18 21:34:16 +0300

    completed introduce slicing

---
+++ src/components/Introduce/Introduce.tsx
@@ -0,0 +1,47 @@
+import React, {useMemo} from "react";
+import IntroduceContent from "./IntroduceContent";
+import styles from "./Introduce.module.scss";
+import classNames from "classnames";
+
+const Introduce = () => {
+  const contentList = useMemo(() => [
+    {
+      image: "../../../public/Mockup-3.svg",
+      title: "Learn from Healthcare Experts",
+      description:
+        "See posts from board-certified experts and ask them questions. Learn how to handle symptoms and care for your well-being. ",
+      buttonTitle: "Get Expert Opinions",
+      onClick: () => {},
+    },
+    {
+      image: "../../../public/Mockup-3.svg",
+      title: "Get Support from the Community",
+      description:
+        "Connect with people who understand the challenges you’re experiencing. Ask questions, swap advice, and learn what works for others.",
+      buttonTitle: "Find Support",
+      onClick: () => {},
+    },
+    {
+      image: "../../../public/Mockup-3.svg",
+      title: "Access Personalized Content",
+      description:
+        "Twill Care’s informative content helps you understand your health concerns and discover remedies and treatments that work.",
+      buttonTitle: "Explore",
+      onClick: () => {},
+    },
+  ],[])
+
+  return (
+    <div>
+      {contentList.map((item, index) => (
+        <IntroduceContent
+          key={`introduceContent_${index}`}
+          className={classNames({ [styles.reversedBlock]: index % 2 > 0 })}
+          {...item}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Introduce;
---
+++ src/components/Introduce/IntroduceContent/IntroduceContent.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import classNames from "classnames";
+
+import "./IntroduceContent.scss";
+
+type IntroduceContentProps = {
+  className?: string;
+  image: string;
+  title: string;
+  description: string;
+  buttonTitle: string;
+  onClick: () => void;
+};
+
+const IntroduceContent: FC<IntroduceContentProps> = ({
+  className,
+  image,
+  title,
+  description,
+  buttonTitle,
+  onClick,
+}) => {
+  return (
+    <div
+      className={classNames(
+        "frame-wrap flex justify-center align-center down-xs:flex-col",
+        className
+      )}
+    >
+      <img className="frame-img" src={image} alt="" />
+
+      <div className="frame flex flex-col justify-between">
+        <div className="frame-header">{title}</div>
+        <div className="frame-content">{description}</div>
+        <button className="frame-button" onClick={onClick}>
+          {buttonTitle}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default IntroduceContent;
